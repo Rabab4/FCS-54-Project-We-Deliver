27077ee0abb195feb6d23a725ec3ebcd8602cb47
Rabab Kawtharani <rak102@mail.aub.edu>
2024-07-15 09:40:10 +0300

    Edited Menu function to repeatedly loop until the user chooses to exit

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ def citiesMenu():
 
 #Menu Function
 def menu():
-    print("Hello! Please enter:")
-    print("    1. To go to the drivers' menu")
-    print("    2. To go to the cities' menu")
-    print("    3. To exit the system")
-    choice=int(input())
-    choices=[1,2,3]
-    while choice not in choices:
-        print("Invalid input! Please try again: ")
+    stop=False
+    while not stop:
+        print("Hello! Please enter:")
+        print("    1. To go to the drivers' menu")
+        print("    2. To go to the cities' menu")
+        print("    3. To exit the system")
         choice=int(input())
-    if choice==1:
-        print(driversMenu())
-    elif choice ==2:
-        print(citiesMenu())
-    else:
-        print("The system is exiting. Good bye!")
-        stop= True
+        choices=[1,2,3]
+        while choice not in choices:
+            print("Invalid input! Please try again: ")
+            choice=int(input())
+        if choice==1:
+            print(driversMenu())
+        elif choice ==2:
+            print(citiesMenu())
+        else:
+            print("The system is exiting. Good bye!")
+            stop= True
 
 menu()
 
