6fa96595107501ead2438b8bcc922e0de033f8e8
Rabab Kawtharani <rak102@mail.aub.edu>
2024-07-15 09:56:11 +0300

    Added Drivers' Menu

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
 # Drivers' Menu
 def driversMenu():
-    message="This is the drivers' menu"
+    stop=False
+    while not stop:
+        print("Hello! This is the Drivers' Menu.")
+        print("Please enter:")
+        print("    1. To view all drives")
+        print("    2. To add a driver")
+        print("    3. To go back to the main menu")
+        choice=int(input())
+        choices=[1,2,3]
+        while choice not in choices:
+            print("Invalid input! Please try again: ")
+            choice=int(input())
+        if choice==1:
+            print(viewDrivers())
+        elif choice ==2:
+            print(addDriver())
+        else:
+            print("Back to main menu")
+            stop= True
+            
+#View Drivers
+def viewDrivers():
+    message="this function shows a list of all the drivers and their detail is printed to the users"
     return message
 
+#Add a Driver
+def addDriver():
+    message="this function enables the user to add the name and start city of the driver to the system"
+    return message
 
 # Cities' Menu
 def citiesMenu():
@@ -24,12 +50,12 @@ def menu():
             print("Invalid input! Please try again: ")
             choice=int(input())
         if choice==1:
-            print(driversMenu())
+            driversMenu()
         elif choice ==2:
             print(citiesMenu())
         else:
             print("The system is exiting. Good bye!")
             stop= True
 
-menu()
 
+menu()
